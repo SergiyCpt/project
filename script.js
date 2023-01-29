d659f362f1f4749dd43737f78abe55bb71efa9d3
Sergiy <33hantkun33@gmail.com>
2023-01-29 09:29:06 +0200

    Add function

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 "use strict";
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+let numberOfFilms;
+
+function start(){
+  while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+  }
+}
+// start();
+
+function detectPersonalLevel (){
+  if(numberOfFilms < 10){
+    console.log('Просмотрено мало фильмов');
+  }else if(10 > numberOfFilms < 30){
+    console.log('Вы класический кинозритель!');
+  }else if(numberOfFilms > 30){
+    console.log('Вы киноман!');
+  }else {
+    console.log('error');
+  }
+}
+
+// detectPersonalLevel();
 
 
 
@@ -16,13 +37,38 @@ const personalMovieBD = {
   privat : false
 };
 
-const a = prompt("Один из последних просмотренных фильмов?",""),
-     b = prompt("На сколько оцените его?",""),
-     c = prompt("Один из последних просмотренных фильмов?",""),
-     d = prompt("На сколько оцените его?","");
 
-personalMovieBD.movies[a] = b;
-personalMovieBD.movies[c] = d;
 
-console.log(personalMovieBD);
+function rememberMyFIlms(){
+  for(let i = 0; i < 2; i++){
+    const a = prompt("Один из последних просмотренных фильмов?",""),
+       b = prompt("На сколько оцените его?","");
+       if(a===''|| a === null || a.length > 50 || b===''|| b === null){
+        alert("Повторите попытку!")
+        i--;
+       }else{
+        personalMovieBD.movies[a] = b;
+       }
+  }
+}
+
+// rememberMyFIlms();
+
+function writeYourGenres(){
+  for(let i = 0; i <= 2; i++){
+    personalMovieBD.genres[i] = prompt(`Назвите ваш любимый жанр под номером ${i + 1}`, "")
+  }
+}
+writeYourGenres();
+
+
+function showMyBD(hiden){
+  if(!hiden){
+    console.log(personalMovieBD);
+  }else{
+    console.log('Its object private')
+  }
+}
+
+showMyBD(personalMovieBD.privat);
 
